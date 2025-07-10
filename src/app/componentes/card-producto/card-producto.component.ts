1dfd1fd9b411d873b1e4d194e427d520fd076361
Carlos Moises Cruzado Acuña <carlosmoises.cruzadoacuna@emeal.nttdata.com>
2025-07-10 16:11:23 -0500

    feat: agrega componentes desde microservicio Java

---
+++ src/app/componentes/card-producto/card-producto.component.ts
@@ -0,0 +1,8 @@
+import { Component } from @angular/core;
+
+@Component({
+  selector: app-card-producto,
+  templateUrl: ./card-producto.component.html,
+  styleUrls: [./card-producto.component.scss]
+})
+export class CardProductoComponent {}
---
+++ src/app/componentes/tarjeta-usuario/tarjeta-usuario.component.ts
@@ -0,0 +1,8 @@
+import { Component } from @angular/core;
+
+@Component({
+  selector: app-tarjeta-usuario,
+  templateUrl: ./tarjeta-usuario.component.html,
+  styleUrls: [./tarjeta-usuario.component.scss]
+})
+export class TarjetaUsuarioComponent {}
