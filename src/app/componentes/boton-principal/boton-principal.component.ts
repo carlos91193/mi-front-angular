342f3d5c2cf6c022190db4a2c791be3548ac0c87
Carlos Moises Cruzado Acuña <carlosmoises.cruzadoacuna@emeal.nttdata.com>
2025-07-14 20:33:02 -0500

    feat: agrega componentes desde microservicio Java

---
+++ src/app/componentes/boton-principal/boton-principal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-principal',
+  templateUrl: './boton-principal.component.html',
+  styleUrls: ['./boton-principal.component.scss']
+})
+export class BotonPrincipalComponent {}
